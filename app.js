94c7c6cf498af568bac80b0b8b57b6b303e4394c
JeMaris <102559144+jemarisw@users.noreply.github.com>
2022-09-17 20:49:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,58 @@ const main_grid = document.querySelector('.favorites .movies-grid')
 const trending_el = document.querySelector('.trending .movies-grid')
 const popup_container = document.querySelector('.popup-container')
 
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "",
+  build: {
+    outDir: "../dist",
+  },
+});
+
+import { offlineFallback, warmStrategyCache } from 'workbox-recipes';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+
+// Set up page cache
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// Set up asset cache
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
+
+// Set up offline fallback
+offlineFallback({
+  pageFallback: '/offline.html',
+});
 
 function add_click_effect_to_card (cards) {
     cards.forEach(card => {
